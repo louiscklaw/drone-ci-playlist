76ec8c23d3fc1c8b73d7bdc91a89c88c60a9ea90
louiscklaw <louiscklaw@gmail.com>
2020-08-13 18:40:34 +0800

    update,

--- scripts/build_main_drone.sh
+++ scripts/build_main_drone.sh
@@ -8,8 +8,6 @@ sudo rm -rf .config
 
 set -ex
 
-script/check_leak.sh
-
 python3 scripts/build_main_drone.py
 sleep 0.5
 
--- scripts/check_leak.sh
+++ scripts/check_leak.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+set -x
+
+sudo rm -rf .ssh
+sudo rm -rf .npm
+sudo rm -rf .config
+
 set -ex
 
 python3 check-leak/main.py
