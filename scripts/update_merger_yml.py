ff83b28d93b73f1cad6a87b796faccc9dc2ce380
louiscklaw <louiscklaw@gmail.com>
2020-08-13 13:13:11 +0800

    update and test,

--- scripts/update_merger_yml.py
+++ scripts/update_merger_yml.py
@@ -6,7 +6,7 @@ from subprocess import Popen, check_output
 SCRIPTS_DIR=os.path.abspath(os.path.dirname(__file__))
 PROJ_HOME = os.path.abspath(SCRIPTS_DIR+'/..')
 
-SKIP_LIST=['.','.git','scripts','x86-runner-setup','hello-ubuntu','drone-python-helloworld','','hello-merger']
+SKIP_LIST=['.','.git','scripts','x86-runner-setup','hello-ubuntu','drone-python-helloworld','','hello-merger','.local']
 
 def runCommand(command_in):
   return check_output(command_in).decode('utf-8')
@@ -24,5 +24,5 @@ def listAllProjects():
   return proj_dirs
 
 def updateDependsOn(in_yml):
-  in_yml = in_yml.replace('<depends_on>','\n'.join(map(lambda x: '    - {}'.format(x), listAllProjects())))
+  in_yml = in_yml.replace('<depends_on>','\n'.join(map(lambda x: '  - {}'.format(x), listAllProjects())))
   return in_yml
