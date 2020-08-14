855b55e50c1cc32aa9f203f19d79f3daf86de80a
louiscklaw <louiscklaw@gmail.com>
2020-08-14 19:57:31 +0800

    update scripts and test,

--- hello-docker-build/docker_build.sh
+++ hello-docker-build/docker_build.sh
@@ -4,21 +4,25 @@ export DEBIAN_FRONTEND=noninteractive
 
 set -x
 
+apt-get update
 apt-get install -yq software-properties-common
 apt-get install -yq lsb-release
 
 cd hello-docker-build
 
   apt-get remove docker docker-engine docker.io
-  apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+  apt-get install -yq apt-transport-https ca-certificates curl software-properties-common
+
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
-  apt-get update
-  apt-get install -y docker-ce
+  # apt-get update
 
-  docker build -t logickee/drone-docker-builder .
+  apt-get install -yq docker-ce
 
-  docker login
-  docker push
+  # docker build -t logickee/drone-docker-builder .
+
+  # # docker login
+  # # docker push
 
 cd
