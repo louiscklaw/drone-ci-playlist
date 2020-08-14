eb14f6b5d3f8bd37d3fbae13b39d6770f9314390
louiscklaw <louiscklaw@gmail.com>
2020-08-15 01:05:56 +0800

    update and test,

--- hello-docker-build-appium/docker_build.sh
+++ hello-docker-build-appium/docker_build.sh
@@ -8,21 +8,4 @@ apt-get update
 apt-get install -yq software-properties-common
 apt-get install -yq lsb-release
 
-cd hello-docker-build
-
-  apt-get remove docker docker-engine docker.io
-
-  apt-get install -yq apt-transport-https ca-certificates curl software-properties-common
-
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
-  # apt-get update
-
-  apt-get install -yq docker-ce
-
-  # docker build -t logickee/drone-docker-builder .
-
-  # # docker login
-  # # docker push
-
-cd
+docker build .
